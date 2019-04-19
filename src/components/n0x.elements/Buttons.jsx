ab98403fd9b17a727d2f6d17ea2ea7a0e971db53
nox <alk3nox@gmail.com>
2019-04-19 17:20:23 -0400

    update

--- src/components/n0x.elements/Buttons.jsx
+++ src/components/n0x.elements/Buttons.jsx
@@ -1,48 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as Home } from '../../static/home.svg'
+import { ReactComponent as About } from '../../static/about.svg'
+import { ReactComponent as Contact } from '../../static/contact.svg'
 
 const BtnShape = styled.button`
   z-index: 10000;
-  height: 1.75em;
-  width: 1.75em;
+  width: 8em;
+  height: 8em;
   margin: .25em;
-  text-align: center;
-  border-radius: 2em;
-  border-width: 0.05em;
-  border-color: rgba(52, 152, 255, 0.8);
+  border-radius: 100%;
+  border-width: 0;
   background-color: rgba(0, 0, 0, 0.8);
   cursor: pointer;
-  font-size: 1.75em;
   opacity: 0.3;
   transition: all 0.25s ease-in-out;
   &:hover,
   &:focus {
-    font-size: 2em;
     opacity: 0.9;
     transition: all 0.25s ease-in-out;
   }
 `;
 
 export const HomeBtn = () => (
-  <BtnShape>
-    <span role="img" title="Home" aria-label="Home">
-      &#127962;
-    </span>
+  <BtnShape role="img" title="Home" aria-label="Home" >
+    <Home/>
   </BtnShape>
 );
 
 export const AboutBtn = () => (
-  <BtnShape>
-    <span role="img" title="About Us" aria-label="About Us">
-      &#128211;
-    </span>
+  <BtnShape role="img" title="About Us" aria-label="About Us" >
+    <About/>
   </BtnShape>
 );
 
 export const ContactBtn = () => (
-  <BtnShape>
-    <span role="img" title="Contact Us" aria-label="Contact Us">
-      &#128231;
-    </span>
+  <BtnShape role="img" title="Contact Us" aria-label="Contact Us" >
+    <Contact/>
   </BtnShape>
 );
