e28a39e073ff9329641fc76bb534d20aa4c9e14d
KCypher <kriss@alkeno-me.com>
2019-08-04 17:57:29 -0400

    Update Home.tsx

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -6,10 +6,12 @@ import { List, Item, P, H1, H2, H3, H4 } from '../components/atoms/text';
 import bg from '../static/images/bg-1.png';
 import logo from '../static/images/logo-landscape.png';
 import Slider from '../components/Slider';
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 
-const graphics = 'https://media.giphy.com/media/8lNgT2uFKdFDh7b2l2/source.gif';
+const graphics = 'https://media.giphy.com/media/MVgLYGeuRP4JynEhNU/source.gif';
 const vfx = 'https://media.giphy.com/media/2w5OEVjSb7f3Y9UQRr/source.gif';
-const vidpro = 'https://media.giphy.com/media/229roNqcyGIZNfWCfj/source.gif';
+const vidpro = 'https://media.giphy.com/media/8JQ9uF8Oe1Ktmmdd8o/source.gif';
 
 const Home: React.FC<{}> = () => {
     let shadow =
@@ -30,7 +32,7 @@ const Home: React.FC<{}> = () => {
                             Media & Entertainment, LLC.
                         </H1>
                     </Box>
-                    <P p={'1em 0'} m={'0'} lineheight={'1.4em'}>
+                    <P p={'1em 0'} m={'0 0 5vh 0'} lineheight={'1.4em'}>
                         We use art, story, and technology to enhance and differentiate your brand from the rest.
                         Everywhere you look, people are having digital experiences on computers, laptops, pads, cellular
                         devices, TVs, and gaming systems. It has become the way of life, to explore the world digitally,
@@ -42,12 +44,12 @@ const Home: React.FC<{}> = () => {
                         <Box
                             p={'0em'}
                             m={'0em'}
-                            width={'80vw'}
-                            height={'50vh'}
+                            width={'80%'}
+                            height={'80%'}
                             bg={`url(${vfx})  scroll`}
                             bgposition={'center center'}
                         >
-                            <Box m={'30vh 0 0 0'}>
+                            <Box m={'45vh 0 0 0'}>
                                 <H4 p={'0'} m={'0'} tshadow={shadow}>
                                     Title Sequences
                                 </H4>
@@ -57,14 +59,14 @@ const Home: React.FC<{}> = () => {
                         <Box
                             p={'0em'}
                             m={'0em'}
-                            width={'80vw'}
-                            height={'50vh'}
+                            width={'80%'}
+                            height={'80%'}
                             bg={`url(${vidpro})  scroll`}
                             bgposition={'center center'}
                         >
-                            <Box m={'30vh 0 0 0'}>
+                            <Box m={'45vh 0 0 0'}>
                                 <H4 p={'0'} m={'0'} tshadow={shadow}>
-                                    sdfasdf
+                                    3D Animation & Sound Design
                                 </H4>
                             </Box>
                         </Box>
@@ -72,16 +74,75 @@ const Home: React.FC<{}> = () => {
                         <Box
                             p={'0em'}
                             m={'0em'}
-                            width={'80vw'}
-                            height={'50vh'}
+                            width={'80%'}
+                            height={'80%'}
                             bg={`url(${graphics})  scroll`}
                             bgposition={'center center'}
                         >
-                            <Box m={'30vh 0 0 0'}>
+                            <Box m={'45vh 0 0 0'}>
                                 <H4 p={'0'} m={'0'} tshadow={shadow}>
                                     Animated GIFs
                                 </H4>
                             </Box>
+                        </Box>
+
+                        <Box
+                            p={'0em'}
+                            m={'0em'}
+                            width={'80%'}
+                            height={'100%'}
+                            style={{ background: 'rgba(0, 0, 0, 0)' }}
+                        >
+                            <ReactPlayer
+                                url="https://github.com/Alkenox-ME/alkenox-me-videos/blob/master/trailer-mosaic.mp4?raw=true"
+                                style={{ background: 'rgba(0, 0, 0, 0)' }}
+                                width={'80%'}
+                                height={'100%'}
+                                volume={'.6'}
+                                controls={'true'}
+                            />
+                            <H4 p={'0'} m={'25vh 0 0 0'} tshadow={shadow} position={'absolute'}>
+                                "Mosaic" Movie Trailer
+                            </H4>
+                        </Box>
+
+                        <Box p={'0em'} m={'0em'} width={'100%'} height={'100%'}>
+                            <ReactPlayer
+                                url="https://vimeo.com/163174806"
+                                width={'80%'}
+                                height={'100%'}
+                                volume={'.6'}
+                                controls={'true'}
+                            />
+                            <H4 p={'0'} m={'25vh 0 0 0'} tshadow={shadow} position={'absolute'}>
+                                "Robin Goodfellow" Movie Trailer
+                            </H4>
+                        </Box>
+
+                        <Box p={'0em'} m={'0em'} width={'100%'} height={'100%'}>
+                            <ReactPlayer
+                                url="https://github.com/Alkenox-ME/alkenox-me-videos/blob/master/artefaektsLogoSequence.mp4?raw=true"
+                                width={'80%'}
+                                height={'100%'}
+                                volume={'.6'}
+                                controls={'true'}
+                            />
+                            <H4 p={'0'} m={'25vh 0 0 0'} tshadow={shadow} position={'absolute'}>
+                                3D Animated Logo
+                            </H4>
+                        </Box>
+
+                        <Box p={'0em'} m={'0em'} width={'100%'} height={'100%'}>
+                            <ReactPlayer
+                                url="https://github.com/Alkenox-ME/alkenox-me-videos/blob/master/The_Smile_Solution_Whitboard.mp4?raw=true"
+                                width={'80%'}
+                                height={'100%'}
+                                volume={'.6'}
+                                controls={'true'}
+                            />
+                            <H4 p={'0'} m={'25vh 0 0 0'} tshadow={shadow} position={'absolute'}>
+                                Whiteboard Animation
+                            </H4>
                         </Box>
                     </Slider>
 
@@ -89,6 +150,8 @@ const Home: React.FC<{}> = () => {
                         .
                     </P>
                 </Box>
+
+                <Box></Box>
             </DeskFrame1>
         </PageLayout>
     );
