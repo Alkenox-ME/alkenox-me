731398341d081a04ded7cc53c364984799eb767a
KCypher <kriss@alkeno-me.com>
2019-08-24 10:44:53 -0400

    fixed

--- src/lib/meta.js
+++ src/lib/meta.js
@@ -1,17 +1,17 @@
 export const meta = {
-    title: 'Alkenox Media & Antertainment, LLC.',
-    url: 'alkenox-me.com',
-    description: 'Alkenox Media & Antertainment, LLC.',
-    keywords:
-        'alkenox, media, entertainment, video production, audio production, content creation, audio book production, graphic design, branding, motion graphics, animation, white board,',
-    favicon: {
-        pngIcon: '',
-        pngAppleTouch: '',
-        svgSafari: '',
-        svgColor: '',
-    },
-    copyright: {
-        year: '2019',
-        company: 'Alkenox Media & Entertainment, LLC.',
-    },
+  title: "Alkenox Media & Entertainment, LLC",
+  url: "alkenox-me.com",
+  description: "Alkenox Media & Entertainment, LLC.",
+  keywords:
+    "alkenox, media, entertainment, video production, audio production, content creation, audio book production, graphic design, branding, motion graphics, animation, white board,",
+  favicon: {
+    pngIcon: "",
+    pngAppleTouch: "",
+    svgSafari: "",
+    svgColor: ""
+  },
+  copyright: {
+    year: "2019",
+    company: "Alkenox Media & Entertainment, LLC."
+  }
 };
