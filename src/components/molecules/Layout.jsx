3065e8a7b5053644e87fc69276b0e0b63d5f47b3
KCypher <kriss@alkeno-me.com>
2019-08-24 14:19:11 -0400

    Update Layout.jsx

--- src/components/molecules/Layout.jsx
+++ src/components/molecules/Layout.jsx
@@ -1,23 +1,23 @@
 // eslint-disable-next-line
 // @Flow
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
 
-import Seo from './Seo';
-import Footer from '../Footer';
-import GlobalStyle from '../theme/GlobalStyle';
-import Navbar from '../Navbar';
+import Seo from "./Seo";
+import Footer from "../Footer";
+import GlobalStyle from "../theme/GlobalStyle";
+import Navbar from "../Navbar";
 
 const Layout = ({ children }) => {
-    return (
-        <div>
-            <Seo />
-            <GlobalStyle />
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Seo />
+      <GlobalStyle />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
